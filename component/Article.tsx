472b6524cf173193f16bce1cbf6eeff082618d06
‘keinn51’ <soogood97@gmail.com>
2022-09-15 10:21:42 +0900

    FEAT: Aricle 내용

--- component/Article.tsx
+++ component/Article.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
-const Article = () => {
+const Article = (props: { categoryTitle: string }) => {
+    const { categoryTitle } = props;
     return (
         <div className='article'>
-            <div className='article-header'></div>
+            <div className='article-header'>{categoryTitle}</div>
             <div className='article-section'>
                 <div className='article-section-content'>
-                    <div className='article-section-content-header'></div>
-                    <div className='article-section-content-title'></div>
+                    <div className='article-section-content-header'>
+                        <div>
+                            <div>QUV</div>
+                            <div>14일전</div>
+                        </div>
+                        <div>
+                            <div>6추</div>
+                            <div>10댓</div>
+                        </div>
+                    </div>
+                    <div className='article-section-content-title'>
+                        QUV 떡상 가즈아
+                    </div>
                 </div>
             </div>
         </div>
--- component/Main.tsx
+++ component/Main.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
+import Article from './Article';
 
 const Main = () => {
+  const ArticleCategoryArr = ["EDITOR'S CHOICE", "WEEKLY BEST", "Q&A", "KNOWLEDGE"];
   return (
     <>
       <div>
         <div className='header'>
-          <div className='header-logo'>hey</div>
-          <div className='header-auth_box'></div>
+          <div className='header-logo'>Quv Board</div>
+          <div className='header-auth_box'>
+            <div>로그인</div>
+            <div>회원가입</div>
+          </div>
         </div>
       </div>
       <div>
         <div className='section'>
-          <div className='section-category'></div>
+          <div className='section-category'>
+            <div>Q&A</div>
+            <div>KNOWLEDGE</div>
+            <div>EVENTS</div>
+            <div>NOTICE</div>
+            <div>JOBS</div>
+          </div>
           <div className='section-articles'>
-            <div className='section-articles-article'>
-              <div className='section-articles-article-header'></div>
-              <div className='section-articles-article-section'>
-              </div>
-            </div>
-            <div className='section-articles-article'></div>
-            <div className='section-articles-article'></div>
-            <div className='section-articles-article'></div>
+            {ArticleCategoryArr.map((title) => <Article categoryTitle={title} />)}
           </div>
         </div>
       </div>
