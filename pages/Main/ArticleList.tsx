e7b26c6c7f4b0a81ff4dae0bb31183c11a6eaa01
‘keinn51’ <soogood97@gmail.com>
2022-09-15 17:13:42 +0900

    FEAT: articleList 에서 article로 넘어가기

--- pages/Main/ArticleList.tsx
+++ pages/Main/ArticleList.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import "@css/Main/Article.css";
+import "@css/Main/ArticleList.css";
 
-const ArticleList = (props: { categoryTitle: string }) => {
-    const { categoryTitle } = props;
+const ArticleList = (props: {
+    categoryTitle: string,
+    setArticleId: React.Dispatch<React.SetStateAction<number | null>>,
+    setCategoryId: React.Dispatch<React.SetStateAction<number | null>>
+}) => {
+    const { categoryTitle, setArticleId,setCategoryId } = props;
+    const onClickArticle = (id:number) => {
+        setArticleId(42);
+        setCategoryId(null);
+    }
     return (
-        <div className='article'>
-            <div className='article-header'>
+        <div className='article_list'>
+            <div className='article_list-header'>
                 <div className='flex_vertical_middle'>
                     <div>
                         {categoryTitle}
                     </div>
                 </div>
             </div>
-            <div className='article-section'>
-                <div className='article-section-content'>
-                    <div className='article-section-content-header'>
+            <div className='article_list-section'>
+                <div className='article_list-section-content'>
+                    <div className='article_list-section-content-header'>
                         <div className='grid_10px_gap'>
                             <div>QUV</div>
                             <div>14일전</div>
@@ -24,7 +32,7 @@ const ArticleList = (props: { categoryTitle: string }) => {
                             <div>10댓</div>
                         </div>
                     </div>
-                    <div className='article-section-content-title'>
+                    <div className='article_list-section-content-title' onClick={() => onClickArticle(42)}>
                         QUV 떡상 가즈아
                     </div>
                 </div>
--- pages/Main/index.tsx
+++ pages/Main/index.tsx
@@ -1,16 +1,40 @@
-import React, { useEffect } from 'react';
-import ArticleList from './ArticleList';
-import "@css/Main/Main.css";
+import React, { useCallback, useEffect } from 'react';
 import { useState } from 'react';
+import ArticleList from './ArticleList';
+import Article from './Article';
+import "@css/Main/Main.css";
 
 const Main = () => {
   const [pageMode, setPageMode] = useState('main');
   const [categoryId, setCategoryId] = useState<number | null>(null);
+  const [articleId, setArticleId] = useState<number | null>(null);
   const ArticleCategoryArr = ["EDITOR'S CHOICE", "WEEKLY BEST", "Q&A", "KNOWLEDGE"];
 
+  const onClickSideCategory = (id: number) => {
+    setCategoryId(id);
+    setArticleId(null);
+  }
+
+  const getRenderingContent = useCallback(() => {
+    if (pageMode === 'main')
+      return (
+        <div className='section-articles'>
+          {ArticleCategoryArr.map((title) => <ArticleList key={title} categoryTitle={title} setArticleId={setArticleId} setCategoryId={setCategoryId}/>)}
+        </div>
+      )
+    else if (pageMode === 'article_list')
+      return (<ArticleList categoryTitle={ArticleCategoryArr[categoryId as number]} setArticleId={setArticleId} setCategoryId={setCategoryId}/>)
+    else
+      return (<Article articleId={articleId}></Article>)
+  }, [pageMode, categoryId, articleId]);
+
   useEffect(() => {
-    if (categoryId !== null) setPageMode('category');
-  }, [categoryId])
+    if (articleId !== null) setPageMode('article');
+    if (categoryId !== null) setPageMode('article_list');
+  }, [articleId, categoryId])
+
+  // console.log(categoryId);
+  // console.log(articleId);
 
   return (
     <div className='section'>
@@ -18,18 +42,11 @@ const Main = () => {
         <div className='section-category-title'>Categories</div>
         <div className='section-category-links'>
           {ArticleCategoryArr.map((title, id) => <div key={title} >
-            <span className='hover_pointer' onClick={() => setCategoryId(id)}>{title}</span>
+            <span className='hover_pointer' onClick={() => onClickSideCategory(id)}>{title}</span>
           </div>)}
         </div>
       </div>
-      {pageMode === 'main' ?
-        <div className='section-articles'>
-          {ArticleCategoryArr.map((title) => <ArticleList key={title} categoryTitle={title} />)}
-        </div>
-        :
-        <div>
-          <ArticleList categoryTitle={ArticleCategoryArr[categoryId as number]} />
-        </div>}
+      {getRenderingContent()}
     </div>
   );
 }
