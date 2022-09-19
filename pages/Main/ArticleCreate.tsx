939959a53ed1baafd1e500c268ce4e82cc1912ae
‘keinn51’ <soogood97@gmail.com>
2022-09-19 12:07:10 +0900

    FEAT: 게시글 생성 API 수정

--- pages/Main/ArticleCreate.tsx
+++ pages/Main/ArticleCreate.tsx
@@ -3,22 +3,39 @@ import '@css/Main/ArticleCreate.css';
 import { getAuth } from '@cert/AuthStorage';
 import { AuthStorageType } from '@globalObj/types';
 import { useState } from 'react';
+import axios from 'axios';
 
 const ArticleCreate = () => {
   const [articleTitle, setArticleTitle] = useState('');
   const [articleContent, setArticleContent] = useState('');
-  const [categoryId, setCategoryId] = useState(-1);
+  const [categoryId, setCategoryId] = useState(0);
 
-  const onClickSubmit = () => {};
+  const onClickSubmit = () => {
+    if (getAuth()) {
+      axios
+        .post(`http://${process.env.IP_ADDRESS}/board/write.php`, {
+          title: articleTitle,
+          content: articleContent,
+          categoryId,
+          userId: (getAuth() as AuthStorageType)['userId'],
+        })
+        .then((res) => {
+          alert('게시글이 생성되었습니다.');
+          location.reload();
+          console.log(res);
+        })
+        .catch((error) => {
+          alert(error.response.data);
+          console.log(error);
+        });
+    }
+  };
 
   return (
     <div className="article_create">
       <div className="article_create-header">
         <p className="font-25">당신의 생각을 들려주세요</p>
-        <p>
-          {(getAuth() as AuthStorageType)['loginId']}님 지식공유 플랫폼 OKKY에서 최고의 개발자들과 함께 궁금증을
-          해결하세요.
-        </p>
+        <p>{(getAuth() as AuthStorageType)['loginId']}님 Squares에서 최고의 팀원들과 함께하세요.</p>
       </div>
       <div className="article_create-title">
         <p className="font-18">제목</p>
