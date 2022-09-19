b9c5a1db75e3aeed9a3a3d99fbdba2a23dc4578a
‘keinn51’ <soogood97@gmail.com>
2022-09-19 11:45:31 +0900

    FEAT: 게시글 생성 자료 준비

--- pages/Main/ArticleCreate.tsx
+++ pages/Main/ArticleCreate.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import '@css/Main/ArticleCreate.css';
 import { getAuth } from '@cert/AuthStorage';
 import { AuthStorageType } from '@globalObj/types';
+import { useState } from 'react';
 
 const ArticleCreate = () => {
+  const [articleTitle, setArticleTitle] = useState('');
+  const [articleContent, setArticleContent] = useState('');
+  const [categoryId, setCategoryId] = useState(-1);
+
+  const onClickSubmit = () => {};
+
   return (
     <div className="article_create">
       <div className="article_create-header">
@@ -15,14 +22,37 @@ const ArticleCreate = () => {
       </div>
       <div className="article_create-title">
         <p className="font-18">제목</p>
-        <textarea className={`article_create-title_textarea`} cols={50} placeholder="제목을 입력해주세요" />
+        <textarea
+          className={`article_create-title_textarea`}
+          onChange={(e) => setArticleTitle(e.target.value)}
+          cols={50}
+          placeholder="제목을 입력해주세요"
+        />
+      </div>
+      <div className="article_create-category">
+        <p className="font-18">카테고리</p>
+        <form>
+          <select onChange={(e) => setCategoryId(parseInt(e.target.value, 10))}>
+            <option value={0}>EDITOR'S CHOICE</option>
+            <option value={1}>WEEKLY BEST</option>
+            <option value={2}>Q&A</option>
+            <option value={3}>KNOWLEDGE</option>
+          </select>
+        </form>
       </div>
       <div className="article_create-content">
         <p className="font-18">상세정보</p>
-        <textarea className={`article_create-content_textarea`} rows={10} placeholder="내용을 입력해주세요" />
+        <textarea
+          className={`article_create-content_textarea`}
+          onChange={(e) => setArticleContent(e.target.value)}
+          rows={10}
+          placeholder="내용을 입력해주세요"
+        />
       </div>
       <div className="flex_horizontal_end">
-        <div className="article_create-create_btn flex_vertical_middle button">게시글 생성</div>
+        <div className="article_create-create_btn flex_vertical_middle button" onClick={onClickSubmit}>
+          게시글 생성
+        </div>
       </div>
     </div>
   );
