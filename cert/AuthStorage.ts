56d6a97bf05be65261ddee9166604be8480c9242
‘keinn51’ <soogood97@gmail.com>
2022-09-19 14:50:02 +0900

    FEAT: auth storage expire 구현

--- cert/AuthStorage.ts
+++ cert/AuthStorage.ts
@@ -3,17 +3,22 @@ import { AuthStorageType, LoginType } from "@globalObj/types";
 const AUTH = 'auth';
 const expireDays = 3;
 
+export function clearAuth() {
+    localStorage.clear();
+}
+
 export function saveAuth(token: LoginType) {
     localStorage.setItem(AUTH, JSON.stringify({...token, expire:  Date.now() + expireDays * 24 * 60 * 60 * 1000}));
 }
 
 export function getAuth(): AuthStorageType | null {
-    if (localStorage.getItem(AUTH))
-        return JSON.parse(localStorage.getItem(AUTH) as string);
-    else
+    if (!localStorage.getItem(AUTH))
         return null;
+    const authObj = JSON.parse(localStorage.getItem(AUTH) as string);
+    if (Date.now() > authObj['expire']) {
+        clearAuth();
+        return null;
+    }
+    return authObj;
 }
 
-export function clearAuth() {
-    localStorage.clear();
-}
